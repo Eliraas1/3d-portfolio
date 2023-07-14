3c692d51c4b637d521c51b8a502ef33ee655337f
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-07-14 18:46:31 +0300

    init

---
+++ src/components/canvas/index.ts
@@ -0,0 +1,6 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computer";
+import StarsCanvas from "./Stars";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
