ce5b34a00b8f6938ade9adee5f02a7b3b9792681
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-07-16 17:47:23 +0300

    complete projects sections

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+import { fadeIn } from "../utils/motion";
+import { github, googlePlay, youtube } from "../assets";
+
+interface ProjectCardProps {
+  name: string;
+  description: string;
+  tags: {
+    name: string;
+    color: string;
+  }[];
+  image: string;
+  source_code_link: string;
+  demo?: string;
+  store?: string;
+  index: number;
+}
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  store,
+  demo,
+  index,
+}) => {
+  const handlePress = (url?: string) => () => {
+    if (!url) return;
+    window.open(url, "black");
+  };
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl  sm:w-[360px] w-full "
+      >
+        <div className="relative w-full h-[230px] ">
+          <img src={image} className="w-full h-full object-cover" />
+        </div>
+        <div className="absolute inset-0 flex justify-start items-end flex-col m-3 card-image_hover gap-1">
+          <div
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            onClick={handlePress(source_code_link)}
+          >
+            <img
+              src={github}
+              alt="github"
+              className="object-contain w-1/2 h-1/2"
+            />
+          </div>
+          {store && (
+            <div
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              onClick={handlePress(store)}
+            >
+              <img
+                src={googlePlay}
+                alt="github"
+                className="object-contain w-1/2 h-1/2"
+              />
+            </div>
+          )}
+          {demo && (
+            <div
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              onClick={handlePress(demo)}
+            >
+              <img
+                src={youtube}
+                alt="github"
+                className="object-contain w-1/2 h-1/2"
+              />
+            </div>
+          )}
+        </div>
+        <div className="mt-5">
+          <h3 className="font-bold text-[23px] font-white">{name}</h3>
+          <p className="mt-2 text-[14px] text-secondary">{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags?.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+export default ProjectCard;
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,7 +1,39 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+import ProjectCard from "./ProjectCard";
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>My projects</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="text-secondary mt-3 text-[17px] max-w-3xl leading-[30px]"
+        >
+          Here are some of the cool things I have built or helped to build,
+          showing off my skills and passion in different areas. You can learn
+          more about each project by reading a quick overview, or by clicking on
+          the links to the code, demos and live versions. Enjoy!
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} index={index} />
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Works;
+// export default Works;
+export default SectionWrapper(Works, "");
