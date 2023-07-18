f606c40e487ce5f37992dfcee39b104d109c1d46
Eliraas1 <eliraas1@ac.sce.ac.il>
2023-07-18 17:58:56 +0300

    deploying test

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import {
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="3d-portfolio">
       <div className="relative z-0 bg-primary relative">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center ">
           <Navbar />
